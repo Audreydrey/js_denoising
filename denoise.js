517d8b05ffa0214511244077a195a52ad284ab7c
Nanfeng <Liu@MacBook-Pro.local>
2021-02-17 22:16:41 +0000

    add example of image I/O

--- denoise.js
+++ denoise.js
@@ -1,25 +1,50 @@
-
-// import {
-//     transpose, chain, derivative, e, evaluate, log, pi, pow, round, sqrt
-//   } from 'mathjs';
-
 const { inv, matrix, size, multiply, transpose, sqrt, ones, zeros } = require('mathjs');
 const { exit } = require('process');
 
-var noisySignal = getImgData("rose.png");
 
+// -------------example of reading, altering and writing images
+// get image data including meta data
+var noisyImg = getImgData("rose.png");
+
+// get pixel values
+var noisySignal = noisyImg.data;
 console.log(noisySignal[100]);
+
+// alter pixel values
+for(var i = 10000 ; i < 20000; i++){
+    noisySignal[i] = 0;
+}
+
+// assign altered pixel values to original image
+noisyImg.data = noisySignal;
+
+// out put image
+putImgData("out.png", noisyImg);
 
 var J = [[1, 0]];
 var JT = sqrt(transpose(J));
 
-var lambdaIn = [[0.5]];
+console.log( multiply(JT, J));
+
+var test = 5;
+console.log(transpose(test))
+
+// var lambdaIn = [[0.5]];
 
 
 
-var eta = multiply(multiply(JT, lambdaIn), 18);
-var m1 = ones(size(eta))
-console.log(m1);
+// var eta = multiply(multiply(JT, lambdaIn), 18);
+// var m1 = ones(size(eta));
+// console.log(m1);
+
+
+
+// var t = [0,1];
+// console.log(t[1]);
+// var t_copy = t.concat(); //cat only copy 1 d array
+// t_copy[1] = -9;
+
+// console.log(t[1]);
 
 
 
@@ -35,22 +60,32 @@ function  getImgData(name){
     var  PNG = require("pngjs").PNG;
 
     var data = fs.readFileSync("input/" + name);
-    return PNG.sync.read(data).data;
+    return PNG.sync.read(data);
+}
+
+function  putImgData(name, png){
+    var fs = require("fs");
+
+    var  PNG = require("pngjs").PNG;
+
+    var options = { colorType: 6 };
+    var buffer = PNG.sync.write(png, options);
+    fs.writeFileSync('output' + name, buffer);
 }
 
 
 class VariableNode{
     constructor(variableID, mu, sigma, priorID, leftID, rightID, upID, downID){
     this.variableID = variableID;
-    this.mu = mu;
-    this.sigma = sigma;
+    this.mu = mu; //should be scalar
+    this.sigma = sigma; //should be scalar
     this.priorID = priorID;
     this.leftID = leftID;
     this.rightID = rightID;
     this.upID = upID;
     this.downID = downID;
-    this.out_eta = zeros(size(mu)); 
-    this.out_lambda_prime = zeros(size(sigma));
+    this.out_eta = 0; 
+    this.out_lambda_prime = 0;
     }
 
     getEta() {
@@ -70,8 +105,8 @@ class VariableNode{
     }
 
     beliefUpdate(){
-        var eta_here = matrix([[0.0]]).astype(np.float32)
-        var lambda_prime_here = np.array([[0.0]]).astype(np.float32)
+        var eta_here = matrix([[0.0]])
+        var lambda_prime_here = matrix([[0.0]])
         var factorIDs = [this.priorID, this.leftID, this.rightID, this.upID, this.downID]
 
         var fID;
@@ -118,20 +153,20 @@ class MeasurementNode{
         this.z = z; //pixel value
         this.lambdaIn = lambdaIn;
 
-        var J = [[1, 0]];
-        var JT = transpose(J);
+        var J = 1.0;
+        // var JT = transpose(J);
 
-        this.eta = multiply(multiply(JT, lambdaIn), z);
-        this.lambdaPrime = multiply(multiply(JT, lambdaIn), J);
+        this.eta = lambdaIn * z;
+        this.lambdaPrime = lambdaIn;
         this.variableID = variableID;
-        this.N_sigma = sqrt(lambdaIn[0][0]);
+        this.N_sigma = sqrt(lambdaIn);
         this.variableEta =this.eta;
         this.variableLambdaPrime = this.lambdaPrime;
     }
 
     computeHuberscale(){
-        var h = this.z - variableNodes[this.variableID].getMu()[0][0];
-        var ms = sqrt(h * this.lambdaIn[0][0] * h);
+        var h = this.z - variableNodes[this.variableID].getMu();
+        var ms = sqrt(h * this.lambdaIn * h);
         if(ms > this.N_sigma){
             var k_r = (2 * this.N_sigma) / ms - this.N_sigma ** 2 / (ms ** 2);
             return k_r;
@@ -166,7 +201,7 @@ class SmoothnessNode{
         // These never get changed because they do not depend on the input variables so linearisation always
         // has the same form
         this.lambdaIn = lambdaIn;
-        this.eta = [[0, 0]];
+        this.eta = [[0], [0]];
         this.lambdaPrime = lambdaIn * multiply(transpose(J), J);
 
         // variable messages:
@@ -188,7 +223,45 @@ class SmoothnessNode{
     }
 
     computeHuberScale(){
-        // var h =
+        var h =  variableNodes[this.prevID] - variableNodes[this.afterID];
+        var ms = sqrt(this.lambdaIn * h);
+        if (ms > this.N_sigma){
+            kr = 2 * this.N_sigma / ms - (this.N_sigma ** 2) / (ms ** 2);
+            return kr;
+        }
+        return 1;
+    }
+
+    //  specify if it is msg to after node or prev
+    computeMsg(isAfter){
+        var inwardID = this.afterID;
+        if(isAfter) {
+            inwardID = this.prevID;
+        }
+        var inwardEta = variableNodes[inwardID].getEta();
+        var inwardLambda = variableNodes[inwardID].getLambdaPrime();
+
+        var kr = this.computeHuberScale()
+        var eta = this.eta;
+        lambda_prime = np.copy(self.lambda_prime)
+
+        // left is the first variable in eta and i want to marginalise out the second one (right)
+        eta[1] = self.eta[1] + inwardEta
+        lambda_prime[1][1] = self.lambda_prime[1][1] + inwardLambda
+
+        eta = eta * k_R
+        lambda_prime = lambda_prime * k_R
+
+        eta_a = eta[0]
+        eta_b = eta[1]
+        lambda_aa = lambda_prime[0][0]
+        lambda_ab = lambda_prime[0][1]
+        lambda_ba = lambda_prime[1][0]
+        lambda_bb = lambda_prime[1][1]
+
+        self.variable_eta = np.array([eta_a - lambda_ab * 1.0 / lambda_bb * eta_b])
+        self.variable_lambda = np.array([lambda_aa - lambda_ab * 1.0 / lambda_bb * lambda_ba])
+
     }
 
 }
